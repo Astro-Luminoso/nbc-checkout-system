99252a8d5f6734b560eb2ba9dc3264e24b1d63b4
hanbyeol.yang <beuri97@gmail.com>
2026-05-29 17:48:59 +0900

    feat: 엔티티 Members 생성
    - ERD 명시대로 엔티티 생성

---
+++ src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/member/domain/Members.java
@@ -0,0 +1,49 @@
+package dev.nbcsparta.assignment.nbccheckoutsystem.member.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "members")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Getter
+public class Members {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+
+    public Members (String email, String password, String name, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
