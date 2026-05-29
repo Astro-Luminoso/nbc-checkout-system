166bf00759f383837bc59f824293394529b8d256
hanbyeol.yang <beuri97@gmail.com>
2026-05-29 11:24:23 +0900

    init: initial commit

---
+++ src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/NbcCheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.nbcsparta.assignment.nbccheckoutsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcCheckoutSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcCheckoutSystemApplication.class, args);
+    }
+
+}
