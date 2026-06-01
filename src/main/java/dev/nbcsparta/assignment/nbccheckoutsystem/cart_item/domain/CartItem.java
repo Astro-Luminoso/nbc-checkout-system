c8b76d6bded68148c81f10215b7d2a5224725a2b
송이 <springday3355@gmail.com>
2026-06-01 16:47:39 +0900

    feat. @CreatedDate 작동을 위한 리스너& 제약조건 추가

--- src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/cart_item/domain/CartItem.java
+++ src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/cart_item/domain/CartItem.java
@@ -1,20 +1,29 @@
 package dev.nbcsparta.assignment.nbccheckoutsystem.cart_item.domain;
 
-
 import dev.nbcsparta.assignment.nbccheckoutsystem.member.domain.Members;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "cart_items")
+@EntityListeners(AuditingEntityListener.class) // @CreatedDate 작동을 위한 리스너 추가
+@Table( // PRD 6.3 준수 / 복합 유니크 제약조건 추가
+        name = "cart_items",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_member_product",
+                        columnNames = {"member_id", "product_id"}
+                )
+        }
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Cart_Item {
+public class CartItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +31,7 @@ public class Cart_Item {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
-    private Members members; // TODO: Member엔티티로 수정 후 리펙토링 예정
+    private Members members;
 
     // TODO: Product 엔티티 생성 후 @ManyToOne 연관관계로 리팩토링 예정
     @Column(name = "product_id", nullable = false)
@@ -32,9 +41,7 @@ public class Cart_Item {
     private Integer quantity;
 
     @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
 
 }
