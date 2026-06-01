98f7beb06bab45f8b363d7ff947ebc47c09947c9
hanbyeol.yang <beuri97@gmail.com>
2026-06-01 19:33:36 +0900

    feat: add OrderItem Entity
    - Add Relations with other entities
    - define unique constraint in OderItem to avoid having same products in one order

---
+++ src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/order/entity/OrderItem.java
@@ -0,0 +1,46 @@
+package dev.nbcsparta.assignment.nbccheckoutsystem.order.entity;
+
+import dev.nbcsparta.assignment.nbccheckoutsystem.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(
+        name = "order_items",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_order_product",
+                        columnNames = {"order_id", "product_id"}
+                )
+        }
+)
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class OrderItem {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(nullable = false)
+    private Integer quantities;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public OrderItem(Long price, Integer quantities, Order order, Product product) {
+        this.price = price;
+        this.quantities = quantities;
+        this.order = order;
+        this.product = product;
+    }
+
+}
