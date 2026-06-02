727fe28305efaeb1e004dace1357e12f48e29743
minkim11 <alsgur4772@gmail.com>
2026-06-02 15:39:06 +0900

    refactor: 포인트 거래내역 응답 형식 변환
    - 리스트 -> dto에 담아서 반환

--- src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/point/controller/PointController.java
+++ src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/point/controller/PointController.java
@@ -1,6 +1,6 @@
 package dev.nbcsparta.assignment.nbccheckoutsystem.point.controller;
 
-import dev.nbcsparta.assignment.nbccheckoutsystem.point.dto.PointTransactionResponse;
+import dev.nbcsparta.assignment.nbccheckoutsystem.point.dto.PointTransactionListResponse;
 import dev.nbcsparta.assignment.nbccheckoutsystem.point.service.PointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +8,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/points")
@@ -19,9 +17,11 @@ public class PointController {
     private final PointService pointService;
 
     @GetMapping("/transactions")
-    public ResponseEntity<List<PointTransactionResponse>> getPointTransactions(
+    public ResponseEntity<PointTransactionListResponse> getPointTransactions(
             @AuthenticationPrincipal Long memberId) {
-        List<PointTransactionResponse> response = pointService.getPointTransactions(memberId);
+        PointTransactionListResponse response = PointTransactionListResponse.from(
+                pointService.getPointTransactions(memberId)
+        );
         return ResponseEntity.ok(response);
     }
 }
