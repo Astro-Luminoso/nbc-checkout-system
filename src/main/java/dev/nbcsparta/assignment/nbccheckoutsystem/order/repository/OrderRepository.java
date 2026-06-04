dd908ff1a5247e96cc07ee40c047aab1e8bace82
hanbyeol.yang <beuri97@gmail.com>
2026-06-05 02:14:27 +0900

    feat:  implement controller, service, repository method

--- src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/order/repository/OrderRepository.java
+++ src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/order/repository/OrderRepository.java
@@ -1,6 +1,9 @@
 package dev.nbcsparta.assignment.nbccheckoutsystem.order.repository;
 
+import dev.nbcsparta.assignment.nbccheckoutsystem.order.dto.SimpleOrderDetail;
 import dev.nbcsparta.assignment.nbccheckoutsystem.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,4 +21,13 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             WHERE o.id = :id
     """)
     Optional<Order> findOrderInFullDetailById(@Param("id") long id);
+
+    @Query(
+            """
+            SELECT new dev.nbcsparta.assignment.nbccheckoutsystem.order.dto.SimpleOrderDetail
+            (o.id, o.orderStatus, o.totalAmount, o.createdDate)
+            FROM Order o
+            WHERE o.memberId = :memberId
+            """)
+    Page<SimpleOrderDetail> findByMemberIdFormatOfSimpleOrderDetail(@Param("memberId") long memberId, Pageable pageable);
 }
