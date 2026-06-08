429c6f0ff36d173ce7a6d49ae61fc67b32647da8
namdongyeob <ehdduq1324@gmail.com>
2026-06-08 16:20:06 +0900

    refactor: AuthController/ProductController 응답 형식 통일 및 예외 처리 개선

--- src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/product/entity/Product.java
+++ src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/product/entity/Product.java
@@ -1,6 +1,7 @@
 package dev.nbcsparta.assignment.nbccheckoutsystem.product.entity;
 
 import dev.nbcsparta.assignment.nbccheckoutsystem.global.entity.BaseEntity;
+import dev.nbcsparta.assignment.nbccheckoutsystem.order.exception.NotOnSaleException;
 import dev.nbcsparta.assignment.nbccheckoutsystem.product.exception.OutOfStockException;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -48,5 +49,14 @@ public class Product extends BaseEntity {
 
 	public void restoreStockValue(int amount) {
 		this.stockQuantity += amount;
+	}
+
+	public void isSellable(int quantities){
+		if (this.getSaleStatus() != SaleStatus.ON_SALE) {
+			throw new NotOnSaleException();
+		}
+		if (this.getStockQuantity() - quantities < 0) {
+			throw new dev.nbcsparta.assignment.nbccheckoutsystem.order.exception.OutOfStockException();
+		}
 	}
 }
