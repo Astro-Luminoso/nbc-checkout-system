86e0753e6dcccde2e3d75fa4b1517c4067826fe2
hanbyeol.yang <beuri97@gmail.com>
2026-06-09 12:59:21 +0900

    refactor: apply new service class into order facade

--- src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/cart_item/service/CartItemValidator.java
+++ src/main/java/dev/nbcsparta/assignment/nbccheckoutsystem/cart_item/service/CartItemValidator.java
@@ -1,10 +1,14 @@
 package dev.nbcsparta.assignment.nbccheckoutsystem.cart_item.service;
 
+import dev.nbcsparta.assignment.nbccheckoutsystem.auth.exception.UnauthorisedException;
+import dev.nbcsparta.assignment.nbccheckoutsystem.cart_item.entity.CartItem;
 import dev.nbcsparta.assignment.nbccheckoutsystem.cart_item.exception.InvalidQuantityException;
 import dev.nbcsparta.assignment.nbccheckoutsystem.cart_item.exception.OutOfStockException;
 import dev.nbcsparta.assignment.nbccheckoutsystem.product.entity.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -18,5 +22,12 @@ public class CartItemValidator {
             throw new OutOfStockException();
         }
     }
+
+    public void inspectCartItemOwner(List<CartItem> cartItems, long memberId) {
+        if (cartItems.stream().anyMatch(item -> item.getMember().getId() != memberId)) {
+            throw new UnauthorisedException();
+        }
+    }
+
 
 }
